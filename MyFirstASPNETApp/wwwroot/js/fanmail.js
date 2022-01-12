7ccf88e0bc20feff516bf41ec472241b5307e2de
kaylajt-1 <kayla.telford@byu.edu>
2022-01-11 22:00:44 -0700

    Add project files.

---
+++ MyFirstASPNETApp/wwwroot/js/fanmail.js
@@ -0,0 +1,6 @@
+﻿document.getElementById("buttonsend").addEventListener("click", function () {
+    alert("From: " + document.getElementById("txtFrom").value +
+        ", Subject: " + document.getElementById("txtSubject").value +
+        ", Message: " + document.getElementById("txtMessage").value);
+    $("#pic").fadeOut("slow");
+})
